85739d1fe4b116ae129e0ae4cd0d96ee04fb0346
daniloti1 <danilo.ti@hotmail.com>
2018-06-23 19:35:42 -0500

    añadiendo comentarios para entender mejor

--- mips1.asm
+++ mips1.asm
@@ -3,23 +3,22 @@
 .text
 	main:
 		#Creando la pila
-		addi $s0, $zero, 1 #2 elementos
+		addi $s0, $zero, 1 #tamaño = 1
 		addi $t0, $zero, 5 #elemento 1
-		addi $t1, $zero, 3 #elemento 2
-		sw $t0, 0($sp)
-		#sw $t1, 4($sp)	
+		sw $t0, 0($sp)	#guardo el 5 en la pila
 		
-		add $a0, $s0, $zero
-		#lw $t2, 0($sp)
-		add $a1, $zero, $sp
-		addi $a2, $zero, 3
-		jal push
-	
+		#añadiendo el 3 a la pila
+		add $a0, $s0, $zero  #argumento a0 el num de elementos de la pila
+		add $a1, $zero, $sp  #argumento a1 la pila
+		addi $a2, $zero, 3   #argumento a2 el numero a agregar
+		jal push			 #llamando push
+		
+		
+		#sumando
 		lw $t2, ($sp)
-		add $a1, $zero, $t2
+		add $a1, $zero, $t2		
 		lw $t3, 4($sp)
-		add $a2, $zero, $t3
-	
+		add $a2, $zero, $t3	
 		jal suma
 		
 		li $v0, 1
@@ -32,19 +31,19 @@
 		
 	suma:		
 		add $v1, $a1, $a2
-		jr $ra  #return
+		jr $ra
 	
 	push:	 
-		add $s0, $a1, $zero
-		addi $t0, $zero, 0
+		add $s0, $a1, $zero  #copiando la pila a s0
+		addi $t0, $zero, 0   #la i=0 para el for
 		loop:
-			bge $t0, $a0, exit
-			addi $s0, $s0, 4
-			addi $t0, $t0, 1
+			bge $t0, $a0, exit  #for de 0 al tamaño de la pila
+			addi $s0, $s0, 4    #sumo 4 al puntero de la pila
+			addi $t0, $t0, 1 	# i++
 			j loop
 		exit:
-			sw $a2, ($s0)
-			addi $a0, $a0, 1			
+			sw $a2, ($s0)		#agrego el numero al final de la pila
+			addi $a0, $a0, 1	#tamaño++		
 			
     	
 		jr $ra

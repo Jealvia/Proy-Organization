5a451ad55e9a2c4b16fb0fc094fe7cbe6518843d
Unknown <jealvia@espol.edu.ec>
2018-06-24 15:34:44 -0500

    Se agrego el reingreso de valores luego de la operacion
    Ya realiza la operacion y se reingresa el elemento a la pila

--- mips1.asm
+++ mips1.asm
@@ -99,6 +99,11 @@
 					add $a1,$zero,$t3	#argumento a1 valor a operar
 					add $a2,$zero,$t4	#argumento a2 valor a operar				
 					jal suma		#Llamada a la funcion suma
+					#Se instancian los valores de la funcion
+				   	add $a0, $s0, $zero  #argumento a0 el numero de elementos de la pila
+				   	add $a1, $zero, $s1  #argumento a1 la pila
+				   	add $a2, $zero, $v1  #argumento a2 el numero a agregar
+				        jal push		#llamando push
 					j while			#Se retorna al while
 				      operacion1:
 				      bne $t1,2,operacion2 #Se valida si no se ingreso 2
@@ -124,6 +129,10 @@
 					add $a1,$zero,$t3	#argumento a1 valor a operar
 					add $a2,$zero,$t4	#argumento a2 valor a operar
 					jal resta		#Llamada a la funcion resta
+					add $a0, $s0, $zero  #argumento a0 el numero de elementos de la pila
+				   	add $a1, $zero, $s1  #argumento a1 la pila
+				   	add $a2, $zero, $v1  #argumento a2 el numero a agregar
+				        jal push		#llamando push
 					j while			#Se retorna al while
 				      operacion2:
 				      bne $t1,3,operacion3 #Se valida si no se ingreso 3
@@ -149,6 +158,10 @@
 					add $a1,$zero,$t3	#argumento a1 valor a operar
 					add $a2,$zero,$t4	#argumento a2 valor a operar	
 					jal multiplicacion	#se llama a la funcion multiplicacion
+					add $a0, $s0, $zero  #argumento a0 el numero de elementos de la pila
+				   	add $a1, $zero, $s1  #argumento a1 la pila
+				   	add $a2, $zero, $v1  #argumento a2 el numero a agregar
+				        jal push		#llamando push
 					j while			#Se retorna al while
 				      operacion3:
 				      bne $t1,4,operacion4 #Se valida si no se ingreso 4
@@ -174,6 +187,10 @@
 					add $a1,$zero,$t3	#argumento a1 valor a operar
 					add $a2,$zero,$t4	#argumento a2 valor a operar
 					jal division		#Se llama a la funcion division
+					add $a0, $s0, $zero  #argumento a0 el numero de elementos de la pila
+				   	add $a1, $zero, $s1  #argumento a1 la pila
+				   	add $a2, $zero, $v1  #argumento a2 el numero a agregar
+				        jal push		#llamando push
 					j while			#Se retorna al while
 				      operacion4: 		#Si no se ingreso una opcion valida muestra mensaje
 					#Se muestra el mensaje
@@ -194,25 +211,29 @@
 				         add $a0, $zero, $s0	#argumento a0 el numero de elementos de la pila		
 					 add $a1, $zero, $s1	#argumento a1 la pila
 			 		 jal mostrarPila	#Llamada a la funcion mostrar pila
-			 		 j while
-			 	     elemInsuficiente:
+			 		 j while		#Se retorna al while
+			 	     elemInsuficiente:		#Si no hay ningun elemento se muesta el respectivo mensaje
+			 	        #Se muestra el mensaje
 			 	        li $v0,4
 			 	        la $a0,pilaVacia
 			 	        syscall
-			 	        j while
+			 	        j while			#Se retorna al while
 				case5:
-				   bne $t0,6,case6 #Se valida si el ingreso no es igual a 6
-				      addi $s2,$s2,3
-				      j exitWhile
-				case6:   
+				   bne $t0,6,case6 		#Se valida si el ingreso no es igual a 6
+				      #Se inicia la salida del while
+				      addi $s2,$s2,3		#Se cambia el valor de la bandera ahora igual a 3
+				      j exitWhile		#Se sale del while
+				case6:   			#Si no se ingresa una opcion valida muestra un mensaje
+				   #Se muestra el mensaje
 				   li $v0,4
 			 	   la $a0,opcionInvalida
 			 	   syscall
-			 	   j while															
+			 	   j while			#Se retorna al while													
 		exitWhile:
+		   #Se cierra el programa	
+		   li $v0, 10	
+		   syscall
 		
-		li $v0, 10	
-		syscall
 		
 	suma:		
 		add $v1, $a1, $a2
